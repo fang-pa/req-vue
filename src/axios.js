352d97875fc107413cb02650e6fcfcbe3e558879
FangFoe <fang-pa@hotmail.com>
2023-06-28 12:07:08 +0700

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "req-api.up.railway.app";
+axios.defaults.baseURL = "https://req-api.up.railway.app";
 axios.defaults.headers.common['x-access-token'] = localStorage.getItem('token');
